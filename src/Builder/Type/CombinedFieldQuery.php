35e23dd3ef7b6b9374ae528014fc010b988900ef
Jérôme Tamarelle <jerome@tamarelle.net>
2023-10-17 18:09:58 +0200

    Validate duplicate operators are not used in CombinedFieldQuery

--- src/Builder/Type/CombinedFieldQuery.php
+++ src/Builder/Type/CombinedFieldQuery.php
@@ -4,28 +4,84 @@ declare(strict_types=1);
 
 namespace MongoDB\Builder\Type;
 
-use MongoDB\BSON\Document;
 use MongoDB\BSON\Serializable;
 use MongoDB\Exception\InvalidArgumentException;
 use stdClass;
 
+use function array_is_list;
+use function array_key_exists;
+use function array_key_first;
+use function array_merge;
+use function array_reduce;
+use function count;
 use function get_debug_type;
+use function get_object_vars;
 use function is_array;
 use function sprintf;
+use function str_starts_with;
 
 /**
- * List of filters that apply to the same field path.
+ * List of field query that apply to the same field path.
  */
 class CombinedFieldQuery implements FieldQueryInterface
 {
-    public function __construct(
-        /** @var list<FieldQueryInterface|Serializable|array|stdClass> $fieldQueries */
-        public readonly array $fieldQueries,
-    ) {
-        foreach ($fieldQueries as $fieldQuery) {
-            if (! $fieldQuery instanceof FieldQueryInterface && ! $fieldQuery instanceof Serializable && ! is_array($fieldQuery) && ! $fieldQuery instanceof stdClass) {
-                throw new InvalidArgumentException(sprintf('Expected filters to be a list of %s, %s, array or stdClass, %s given.', FieldQueryInterface::class, Document::class, get_debug_type($fieldQuery)));
+    /** @var list<FieldQueryInterface|Serializable|array|stdClass> $fieldQueries */
+    public readonly array $fieldQueries;
+
+    /** @param list<FieldQueryInterface|Serializable|array|stdClass> $fieldQueries */
+    public function __construct(array $fieldQueries)
+    {
+        if (! array_is_list($fieldQueries)) {
+            throw new InvalidArgumentException('Expected filters to be a list, invalid array given.');
+        }
+
+        // Flatten nested CombinedFieldQuery
+        $this->fieldQueries = array_reduce($fieldQueries, static function (array $fieldQueries, mixed $fieldQuery): array {
+            if ($fieldQuery instanceof CombinedFieldQuery) {
+                return array_merge($fieldQueries, $fieldQuery->fieldQueries);
             }
+
+            $fieldQueries[] = $fieldQuery;
+
+            return $fieldQueries;
+        }, []);
+
+        // Validate FieldQuery types and non-duplicate operators
+        $seenOperators = [];
+        foreach ($this->fieldQueries as $fieldQuery) {
+            if ($fieldQuery instanceof FieldQueryInterface && $fieldQuery instanceof OperatorInterface) {
+                $operator = $fieldQuery->getOperator();
+            } elseif (is_array($fieldQuery)) {
+                if (count($fieldQuery) !== 1) {
+                    throw new InvalidArgumentException(sprintf('Operator array must contain exactly one key starting with $. %d given.', count($fieldQuery)));
+                }
+
+                $operator = array_key_first($fieldQuery);
+                if (! str_starts_with($operator, '$')) {
+                    throw new InvalidArgumentException(sprintf('Operator array must contain exactly one key starting with $. "%s" given.', $operator));
+                }
+            } elseif ($fieldQuery instanceof stdClass) {
+                $fieldQuery = get_object_vars($fieldQuery);
+                if (count($fieldQuery) !== 1) {
+                    throw new InvalidArgumentException(sprintf('Operator object must contain exactly one key starting with $. %d given.', count($fieldQuery)));
+                }
+
+                $operator = array_key_first($fieldQuery);
+                if (! str_starts_with($operator, '$')) {
+                    throw new InvalidArgumentException(sprintf('Operator object must contain exactly one key starting with $. "%s" given.', $operator));
+                }
+            } elseif ($fieldQuery instanceof Serializable) {
+                // Unknown operator, let the server handle it
+                continue;
+            } else {
+                throw new InvalidArgumentException(sprintf('Expected filters to be a list of field query operators, BSON documents, array or stdClass, %s given.', get_debug_type($fieldQuery)));
+            }
+
+            if (array_key_exists($operator, $seenOperators)) {
+                throw new InvalidArgumentException(sprintf('Duplicate operator "%s" detected.', $operator));
+            }
+
+            $seenOperators[$operator] = true;
         }
     }
 }
--- tests/Builder/Type/CombinedFieldQueryTest.php
+++ tests/Builder/Type/CombinedFieldQueryTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace MongoDB\Tests\Builder\Type;
 
 use Generator;
+use MongoDB\BSON\Serializable;
+use MongoDB\Builder\Query\EqOperator;
+use MongoDB\Builder\Query\GtOperator;
 use MongoDB\Builder\Type\CombinedFieldQuery;
 use MongoDB\Exception\InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
@@ -21,9 +24,31 @@ class CombinedFieldQueryTest extends TestCase
     public function testFieldQueries(): void
     {
         $fieldQueries = new CombinedFieldQuery([
-            $this->createMock(CombinedFieldQuery::class),
+            new EqOperator(1),
             ['$gt' => 1],
-            new CombinedFieldQuery([]),
+            (object) ['$lt' => 1],
+            new class implements Serializable {
+                public function bsonSerialize(): array
+                {
+                    return ['$gte' => 1];
+                }
+            },
+        ]);
+
+        $this->assertCount(4, $fieldQueries->fieldQueries);
+    }
+
+    public function testFlattenCombinedFieldQueries(): void
+    {
+        $fieldQueries = new CombinedFieldQuery([
+            new CombinedFieldQuery([
+                new CombinedFieldQuery([
+                    ['$lt' => 1],
+                    new CombinedFieldQuery([]),
+                ]),
+                ['$gt' => 1],
+            ]),
+            ['$gte' => 1],
         ]);
 
         $this->assertCount(3, $fieldQueries->fieldQueries);
@@ -44,5 +69,49 @@ class CombinedFieldQueryTest extends TestCase
         yield 'string' => ['foo'];
         yield 'bool' => [true];
         yield 'null' => [null];
+        yield 'empy array' => [[]];
+        yield 'empy object' => [(object) []];
+        yield 'not operator array' => [['eq' => 1]];
+        yield 'not operator object' => [(object) ['eq' => 1]];
+    }
+
+    /**
+     * @param array<mixed> $fieldQueries
+     *
+     * @dataProvider provideDuplicateOperator
+     */
+    public function testRejectDuplicateOperator(array $fieldQueries): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Duplicate operator "$eq" detected.');
+
+        new CombinedFieldQuery([
+            ['$eq' => 1],
+            new EqOperator(2),
+        ]);
+    }
+
+    public function provideDuplicateOperator(): Generator
+    {
+        yield 'array and FieldQuery' => [
+            [
+                ['$eq' => 1],
+                new EqOperator(2),
+            ],
+        ];
+
+        yield 'object and FieldQuery' => [
+            [
+                (object) ['$gt' => 1],
+                new GtOperator(2),
+            ],
+        ];
+
+        yield 'object and array' => [
+            [
+                (object) ['$ne' => 1],
+                ['$ne' => 2],
+            ],
+        ];
     }
 }
