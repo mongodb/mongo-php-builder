2890017fa72472cb5dfae93ddffa5e2ee94403e0
Jérôme Tamarelle <jerome@tamarelle.net>
2024-01-30 16:53:31 +0100

    Remove $slice operator from projection, only allowed in find command

--- src/Builder/Projection/FactoryTrait.php
+++ src/Builder/Projection/FactoryTrait.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 
 namespace MongoDB\Builder\Projection;
 
-use MongoDB\Builder\Type\Optional;
 use MongoDB\Builder\Type\QueryInterface;
 
 /**
@@ -25,17 +24,5 @@ trait FactoryTrait
     public static function elemMatch(QueryInterface|array $query): ElemMatchOperator
     {
         return new ElemMatchOperator($query);
-    }
-
-    /**
-     * Limits the number of elements projected from an array. Supports skip and limit slices.
-     *
-     * @see https://www.mongodb.com/docs/manual/reference/operator/projection/slice/
-     * @param int $limit
-     * @param Optional|int $skip
-     */
-    public static function slice(int $limit, Optional|int $skip = Optional::Undefined): SliceOperator
-    {
-        return new SliceOperator($limit, $skip);
     }
 }
